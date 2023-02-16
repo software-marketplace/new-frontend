178d8c404c4aafd911f9c5d96513bcc3788d9dec
unknown <priyanka.makhija@mikadosolutions.com>
2023-02-17 02:16:10 +0530

    login/signup

--- src/components/LoginSignup/Login.jsx
+++ src/components/LoginSignup/Login.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../../pages/login-signupmodal/loginSignup.css'
 import CloseIcon from '@mui/icons-material/Close';
 
-export default function Login({setLoginClose,setIsModalOpen}) {
+export default function Login({setLoginClose,setIsModalOpen, setUser}) {
+  const [username,setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = () => {
+    console.log("In handleLogin");
+    setUser({username});
+  }
   return (
+    <form onSubmit={(event)=> event.preventDefault()}>
     <div className='Container'>
         <div className='contentContainer'> 
       <span className='headerSection'>Login/</span><span onClick={() => setLoginClose(false)} className='shiftSection'>SignUp</span>
       <CloseIcon className='closeIcon' onClick={() =>setIsModalOpen(false)} />
       <div className='inputSection'>
-          <input placeholder='Username' className='input'></input>
-          <input placeholder='Password' className='input'></input>
+          <input placeholder='Username' className='input' onChange={(event) => setUsername(event.target.value)}></input>
+          <input placeholder='Password' type="password" className='input' onChange={(event) => setPassword(event.target.value)}></input>
       </div>
       <div className='bottomSection'>
-        <button>LOGIN</button><br/>
+        <button type='submit' onSubmit={handleLogin}>LOGIN</button><br/>
       </div>
       </div>
     </div>
+    </form>
   )
 }
--- src/components/LoginSignup/Signup.jsx
+++ src/components/LoginSignup/Signup.jsx
@@ -2,24 +2,31 @@ import React from 'react'
 import '../../pages/login-signupmodal/loginSignup.css'
 import CloseIcon from '@mui/icons-material/Close';
 
-export default function Signup({setLoginClose,setIsModalOpen}) {
+export default function Signup({setLoginClose,setIsModalOpen, setUser}) {
+  const [username,setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSignup = () => {
+    console.log("In handleSignup");
+    setUser({username});
+  }
   return (
+    <form onSubmit={(event) => event.preventDefault()}>
     <div className='Container1'>
         <div className='contentContainer'> 
       <span className='headerSection'>SignUp/</span><span onClick={() => setLoginClose(true)} className='shiftSection'>Login</span>
       <CloseIcon className='closeIcon'  onClick={() =>setIsModalOpen(false)}  />
       <div className='inputSection'>
           <input placeholder='Name' className='input'></input>
-          <input placeholder='Email' className='input'></input>
-          <input placeholder='Password' className='input'></input>
+          <input placeholder='Username' className='input' onChange={(event) => setUsername(event.target.value)}></input>
+          <input placeholder='Password' type="password" className='input' onChange={(event) => setPassword(event.target.value)}></input>
           <input placeholder='Phone Number' className='input'></input>
-         
       </div>
       <div className='bottomSection'>
-        <button>Signup</button><br/>
+        <button type='submit' onClick={handleSignup}>Signup</button><br/>
      
       </div>
       </div>
     </div>
+    </form>
   )
 }
