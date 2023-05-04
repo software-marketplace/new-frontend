728d81f52c6b38c3f5d3dadf9c6443db16e28433
unknown <priyanka.makhija28@gmail.com>
2023-05-04 08:14:50 +0530

    local

--- src/App.js
+++ src/App.js
@@ -1,35 +1,100 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ProductListing from "./pages/productslisting/ProductListing";
 import SingleProduct from "./pages/productOpen.jsx/SingleProduct";
 import Footer from "../src/components/footer/Footer";
 import ProductsPage from "./pages/productspage/ProductsPage";
+import ProductComparisonTable from "./components/compare/ProductComparisonTable";
+// import ProductContext from "./ProductContext";
+import Faq from "./pages/faq";
 import { useState } from "react";
-import ChatWidget from "./components/ChatWidget/ChatWidget";
-
+// import ProductComparisonTable from './ProductComparisonTable';
 export default function App() {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [currentUser, setCurrentUser] = useState(null);
-    const [openLogin, setOpenLogin] = useState(false);
-    const [isOpen, setIsOpen] = useState(false);
+  const [openChat, setOpenChat] = useState(false);
+  const [users, setUsers] = useState([
+    { email: "priyanka.makhija05@gmail.com", password: "Abc@123!" },
+  ]);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [currentUser, setCurrentUser] = useState(null);
+  const [openLogin, setOpenLogin] = useState(false);
+  const [selectedProducts, setSelectedProducts] = useState([]);
+  const handleProductSelect = (product) => {
+    console.log("in HPS");
+    if (selectedProducts.length >= 3) return false;
+    const newSelectedProducts = [...selectedProducts, product];
+    setSelectedProducts(newSelectedProducts);
+    console.log("New selected products ", selectedProducts);
+    return true;
+  };
 
-    return (
-        <>
-            <div className="h">
-                {localStorage.getItem("access_token") ?
-                    <ChatWidget isOpen={isOpen} setIsOpen={setIsOpen} /> : <></>}
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/">
-                            <Route index element={<ProductListing />} />
-                            <Route path="products" element={<ProductsPage />} />
-                            <Route path="product/:slug" element={<SingleProduct setOpenLogin={setOpenLogin} openLogin={openLogin} currentUser={currentUser} setCurrentUser={setCurrentUser} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />
-                        </Route>
-                    </Routes>
-                    <Footer />
-                </BrowserRouter>
-            </div>
-        </>
+  const handleProductRemove = (product) => {
+    const newSelectedProducts = selectedProducts.filter(
+      (p) => p._id !== product._id
     );
+    setSelectedProducts(newSelectedProducts);
+  };
+
+  const isProductSelected = (product) => {
+    return selectedProducts.some((p) => p._id === product._id);
+  };
+
+  const getSelectedProductCount = () => {
+    return selectedProducts.length;
+  };
+
+  useEffect(() => {
+    console.log("New selected products ", selectedProducts);
+  }, [selectedProducts]);
+
+  return (
+    // <ProductContext.Provider value={[selectedProducts, setSelectedProducts]}>
+    <>
+      {/* <Navbar /> */}
+      <div className="h">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/">
+              <Route index element={<ProductListing />} />
+              <Route path="developer" element={<DeveloperDashbord />} />
+              <Route
+                path="products"
+                element={
+                  <ProductsPage
+                    handleProductRemove={handleProductRemove}
+                    handleProductSelect={handleProductSelect}
+                    isProductSelected={isProductSelected}
+                    getSelectedProductCount={getSelectedProductCount}
+                  />
+                }
+              />
+              <Route
+                path="product/:slug"
+                element={
+                  <SingleProduct
+                    setOpenLogin={setOpenLogin}
+                    openLogin={openLogin}
+                    openChat={openChat}
+                    setOpenChat={setOpenChat}
+                    currentUser={currentUser}
+                    setCurrentUser={setCurrentUser}
+                    isLoggedIn={isLoggedIn}
+                    setIsLoggedIn={setIsLoggedIn}
+                  />
+                }
+              />
+              <Route path="userDashboard" element={<UserDashboard />} />
+              <Route path="faq" element={<Faq />} />
+              <Route
+                path="/product-comparison-table"
+                element={<ProductComparisonTable products={selectedProducts} />}
+              />
+            </Route>
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </div>
+    </>
+    // </ProductContext.Provider>
+  );
 }
--- src/pages/productOpen.jsx/SingleProduct.jsx
+++ src/pages/productOpen.jsx/SingleProduct.jsx
@@ -1,63 +1,116 @@
-import React, { useState } from "react";
-import "./SingleProduct.scss";
-import ChatWidget from "../../components/chat/ChatWidget";
-
-import { useParams } from "react-router-dom";
-import data from "../../devhustle.json";
-import { useEffect } from "react";
 import Review from "../../components/post/Review";
 import Navbar from "../../components/Navbar/Navbar";
 
-// export default function SingleProduct({openChat, setOpenChat, currentUser, setCurrentUser, isLoggedIn, setIsLoggedIn, openLogin, setOpenLogin}) {
+import React, { useState, useEffect } from "react";
+import "./SingleProduct.scss";
+import Navbar2 from "../../components/Navbar/Navbar";
+import ModalImage from "react-modal-image";
+import ImageSection from "../../components/misleneous/imagesDisplay";
+import ChatWidget from "../../components/chat/ChatWidget";
+// import { LightBox } from "react-lightbox-pack";
+// import "react-lightbox-pack/dist/index.css";
+import { useParams } from "react-router-dom";
+import { baseUrl } from "../../config";
+
 export default function SingleProduct() {
-    let params = useParams();
-    console.log("params=", params.slug);
-    console.log("data = ", data[Number(params.slug)]);
-    // const [product, setProduct] = useState({});
-    const [isOpen, setIsOpen] = useState(false);
-    const [imgIndex, setImgIndex] = useState(0);
-    // const [imgIndex, setImgIndex] = useState(0);
-    const [openChat, setOpenChat] = useState(false);
+  const [product, setProduct] = useState(null);
+  const [highlights, setHighlights] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [imgIndex, setImgIndex] = useState(0);
+  const [openChat, setOpenChat] = useState(false);
+  const [toggle, setToggle] = useState(false);
+  const [sIndex, setSIndex] = useState(0);
+  const params = useParams();
 
-    const [toggle, setToggle] = useState(false);
-    const [sIndex, setSIndex] = useState(0);
-    const product = data[Number(params.slug)];
-    console.log("product = ", product);
+  useEffect(() => {
+    fetch(`${baseUrl}/products/${params.slug}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((res2) => {
+        setProduct(res2);
+      })
+      .catch((error) => console.log(error));
+  }, [params.slug]);
 
-    useEffect(() => {
-        // const slug = params.slug;
-        // console.log("id = ",slug);
-        // setProduct(data[slug]);
-    }, []);
+  useEffect(() => {
+    if (product) {
+      let temp = [];
+      if (product.compare.frontend.length > 0)
+        temp.push(
+          "Frontend technologies: " + product.compare.frontend.join(",")
+        );
+      if (product.compare.backend.length > 0)
+        temp.push("Backend technologies: " + product.compare.backend.join(","));
+      if (product.compare.cloud_providers.length > 0)
+        temp.push(
+          "Cloud Providers: " + product.compare.cloud_providers.join(",")
+        );
+      if (product.compare.cloud_Services.length > 0)
+        temp.push(
+          "Cloud Services: " + product.compare.cloud_Services.join(",")
+        );
+      if (product.compare.type) temp.push("Type: " + product.compare.type);
+      if (product.compare.admin.toLowerCase() === "yes")
+        temp.push("Admin module available");
+      if (product.compare.tracking_dashboards.toLowerCase() === "yes")
+        temp.push("Tracking Dashboards available");
+      if (product.compare.navigation_dahboards.toLowerCase() === "yes")
+        temp.push("Login dashboards available");
+      if (product.compare.is_cloud_data_store.toLowerCase() === "yes")
+        temp.push("Data stored in cloud");
+      if (product.compare.databases.length > 0)
+        temp.push("Databases used: " + product.compare.databases.join(","));
+      if (product.compare.architecture.length > 0)
+        temp.push(
+          "Architecture used: " + product.compare.architecture.join(",")
+        );
+      if (product.compare.license.length > 0)
+        temp.push("Licenses available: " + product.compare.license.join(","));
+      if (product.compare.maintainance_lifetime.toLowerCase() === "yes")
+        temp.push("Lifetime Maintanence available");
+      if (product.compare.maintainence_in_months.toLowerCase() !== "na")
+        temp.push(
+          "Maintanence period of " +
+            product.compare.maintainence_in_months +
+            " months"
+        );
+      setHighlights(temp);
+    }
+  }, [product]);
 
-    // Handler
-    const lightBoxHandler = (state, sIndex) => {
-        setToggle(state);
-        setSIndex(sIndex);
-    };
+  const lightBoxHandler = (state, sIndex) => {
+    setToggle(state);
+    setSIndex(sIndex);
+  };
 
-    return (
-        <>
-            <Navbar />
-            {/* <Navbar openChat={openChat} setOpenChat={setOpenChat} currentUser={currentUser} setCurrentUser={setCurrentUser} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} openLogin={openLogin} setOpenLogin={setOpenLogin} /> */}
-
-            <div className="yes">
-                <div className="big">
-                    <article className="recipe bigBox">
-                        <div className="imageHolder">
-                            {product.images.slice(0, 2).map((item, index) => (
-                                <>
-                                    <div className="pizza-box productImage" key={item.id}>
-                                        <img
-                                            src={item}
-                                            className="img"
-                                            onClick={() => {
-                                                lightBoxHandler(true, index);
-                                            }}
-                                            alt=""
-                                        />
-                                    </div>
-                                    {/* <LightBox
+  return (
+    <>
+      <Navbar />
+      {!product && <p>Loading...</p>}
+      {/* <Navbar openChat={openChat} setOpenChat={setOpenChat} currentUser={currentUser} setCurrentUser={setCurrentUser} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} openLogin={openLogin} setOpenLogin={setOpenLogin} /> */}
+      {product && (
+        <div className="yes">
+          <>
+            <div className="big">
+              <article className="recipe bigBox">
+                <div className="imageHolder">
+                  {product.images.slice(0, 2).map((item, index) => (
+                    <>
+                      <div className="pizza-box productImage" key={item.id}>
+                        <img
+                          src={item}
+                          className="img"
+                          onClick={() => {
+                            lightBoxHandler(true, index);
+                          }}
+                          alt=""
+                        />
+                      </div>
+                      {/* <LightBox
                     state={toggle}
                     event={lightBoxHandler}
                     data={data}
@@ -68,92 +121,104 @@ export default function SingleProduct() {
                     setImageIndex={setSIndex}
                     imageIndex={sIndex}
                   /> */}
-                                </>
-                            ))}
-                            {/* <ImageSection lightBoxHandler={lightBoxHandler} /> */}
-                        </div>
-
-                        <div className="recipe-content">
-                            <h1 className="recipe-title">
-                                <a href="#">{product.product_name}</a>
-                            </h1>
-                            <p className="recipe-metadata">
-                                <span className="recipe-rating">★★★★</span>
-                                <span className="recipe-votes">{product.reviews.length}</span>
-                            </p>
-                            <h1 className="recipe-pricetag">{"Rs." + product.Price}</h1>
-                            <button className="recipe-save" type="button" onClick={() => { }}>
-                                Contact Developer
-                            </button>
-                            <p className="recipe-desc">{product.description}</p>
-                            <h2 className="subHeader">Highlights</h2>
-                            <ul className="listedItem">
-                                <li>Admin Dashbard</li>
-                                <li>Highly Cursomizable</li>
-                                <li>Highly Cursomizable</li>
-                                <li>Easy revenue tracking</li>
-                                <li>Admin dashboard</li>
-                                <li>Highly Cursomizable</li>
-                            </ul>
-
-                            <div className="detailSection">
-                                <h2 className="subHeader">Details</h2>
-                                <p>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
-                                    rerum hic ratione numquam quos praesentium dolor a consequatur
-                                    nisi ipsam voluptatem, magnam tempore, aliquid cupiditate
-                                    dolorum impedit unde velit omnis suscipit blanditiis? Nemo
-                                    suscipit qui animi? Neque ipsum consequatur deserunt earum
-                                    iure inventore nam magni, tenetur vel, necessitatibus
-                                    veritatis ullam odit aliquam? Repellendus, at! Dolorem libero
-                                    odio quos tenetur temporibus minus commodi, omnis error?
-                                </p>
-                            </div>
-                            <div className="review">
-                                {product.reviews.map(
-                                    (item, index) => (
-                                        <Review
-                                            rating={item.rating}
-                                            review={item.review}
-                                            id={item.id}
-                                            user={item.user}
-                                        />
-                                    )
-                                    // <Review item={item}/>
-                                )}
-                                {/* {product.reviews} */}
-                            </div>
-                        </div>
-                    </article>
+                    </>
+                  ))}
+                  {/* <ImageSection lightBoxHandler={lightBoxHandler} /> */}
                 </div>
-                <div className="small">
-                    <article className=" side">
-                        <div className="pizza-box side1"></div>
-                        <div className="recipe-content1">
-                            <p className="recipe-tags">
-                                <span className="recipe-tag">Flutter developer</span>
-                                <span className="recipe-tag">Mern Expert</span>
-                            </p>
-                            <h1 className="recipe-title">
-                                <a href="#">Developer Name</a>
-                            </h1>
-                            <p className="recipe-metadata">
-                                {/* <span className="recipe-rating">★★★★<span>☆</span></span> */}
-                                {/* <span className="recipe-votes">(12 votes)</span> */}
-                            </p>
-                            <p className="recipe-desc">
-                                Here we can tell about the developer
-                            </p>
-                            <button className="recipe-save" type="button">
-                                Contact Developer
-                            </button>
-                        </div>
-                    </article>
+
+                <div className="recipe-content">
+                  <h1 className="recipe-title">
+                    <a href="#">{product.product_name}</a>
+                  </h1>
+                  <p className="recipe-metadata">
+                    <span className="recipe-rating">★★★★</span>
+                    <span className="recipe-votes">
+                      {product.reviews.length}
+                    </span>
+                  </p>
+                  <h1 className="recipe-pricetag">{"Rs." + product.Price}</h1>
+                  <button
+                    className="recipe-save"
+                    type="button"
+                    onClick={() => {}}
+                  >
+                    Contact Developer
+                  </button>
+                  <p className="recipe-desc">{product.description}</p>
+                  <h2 className="subHeader">Highlights</h2>
+                  <ul className="listedItem">
+                    {highlights.map((item, index) => (
+                      <li>{item}</li>
+                    ))}
+                    {/* <li>Admin Dashbard</li>
+                <li>Highly Cursomizable</li>
+                <li>Highly Cursomizable</li>
+                <li>Easy revenue tracking</li>
+                <li>Admin dashboard</li>
+                <li>Highly Cursomizable</li> */}
+                  </ul>
+
+                  <div className="detailSection">
+                    <h2 className="subHeader">Details</h2>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Esse rerum hic ratione numquam quos praesentium dolor a
+                      consequatur nisi ipsam voluptatem, magnam tempore, aliquid
+                      cupiditate dolorum impedit unde velit omnis suscipit
+                      blanditiis? Nemo suscipit qui animi? Neque ipsum
+                      consequatur deserunt earum iure inventore nam magni,
+                      tenetur vel, necessitatibus veritatis ullam odit aliquam?
+                      Repellendus, at! Dolorem libero odio quos tenetur
+                      temporibus minus commodi, omnis error?
+                    </p>
+                  </div>
+                  <div className="review">
+                    {product.reviews.map(
+                      (item, index) => (
+                        <Review
+                          rating={item.rating}
+                          comment={item.comment}
+                          id={item.id}
+                          user={item.user}
+                        />
+                      )
+                      // <Review item={item}/>
+                    )}
+                    {/* {product.reviews} */}
+                  </div>
                 </div>
+              </article>
             </div>
-            {openChat && (
-                <ChatWidget username={"priyanka@gmail.com"} devUsername={""} />
-            )}
-        </>
-    );
+            <div className="small">
+              <article className=" side">
+                <div className="pizza-box side1"></div>
+                <div className="recipe-content1">
+                  <p className="recipe-tags">
+                    <span className="recipe-tag">Flutter developer</span>
+                    <span className="recipe-tag">Mern Expert</span>
+                  </p>
+                  <h1 className="recipe-title">
+                    <a href="#">Developer Name</a>
+                  </h1>
+                  <p className="recipe-metadata">
+                    {/* <span className="recipe-rating">★★★★<span>☆</span></span> */}
+                    {/* <span className="recipe-votes">(12 votes)</span> */}
+                  </p>
+                  <p className="recipe-desc">
+                    Here we can tell about the developer
+                  </p>
+                  <button className="recipe-save" type="button">
+                    Contact Developer
+                  </button>
+                </div>
+              </article>
+            </div>
+          </>
+        </div>
+      )}
+      {/* {openChat && (
+        <ChatWidget username={"priyanka@gmail.com"} devUsername={""} />
+      )} */}
+    </>
+  );
 }
